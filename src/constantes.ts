2759ecd5e21acfd02b19a0fce13804c4c2e67b20
Gustavo Riedel <gustavor272@gmail.com>
2025-11-18 12:27:33 -0300

    base url changed para agregar com o pythonAnywhere

--- src/constantes.ts
+++ src/constantes.ts
@@ -1,4 +1,4 @@
 // AcervoApp/src/constantes.ts
 // Constantes globais para acesso em todos os arquivos
-const backendAddress: string = 'http://127.0.0.1:8000/';
+const backendAddress: string = 'https://guguriedel.pythonanywhere.com/';
 const tokenKeyword: string = 'Token ';
