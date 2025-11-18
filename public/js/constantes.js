599ff3912e0064d0177832087e857710d7c402ad
Gustavo Riedel <gustavor272@gmail.com>
2025-11-18 13:30:51 -0300

    Fix backend address to include trailing slash

--- public/js/constantes.js
+++ public/js/constantes.js
@@ -1,6 +1,6 @@
 "use strict";
 // AcervoApp/src/constantes.ts
 // Constantes globais para acesso em todos os arquivos
-const backendAddress = 'https://guguriedel.pythonanywhere.com';
+const backendAddress = 'https://guguriedel.pythonanywhere.com/';
 const tokenKeyword = 'Token ';
 //# sourceMappingURL=constantes.js.map
--- src/constantes.ts
+++ src/constantes.ts
@@ -1,4 +1,4 @@
 // AcervoApp/src/constantes.ts
 // Constantes globais para acesso em todos os arquivos
-const backendAddress: string = 'https://guguriedel.pythonanywhere.com';
+const backendAddress: string = 'https://guguriedel.pythonanywhere.com/';
 const tokenKeyword: string = 'Token ';
